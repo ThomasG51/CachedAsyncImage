a847830ffe5584125680f44adcaabe75a0fc2d14
Thomas George <private@thomas-george.com>
2023-01-28 18:11:22 +0100

    Initial Commit

---
+++ CachedAsyncImage/CachedAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  CachedAsyncImageApp.swift
+//  CachedAsyncImage
+//
+//  Created by Thomas George on 28/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct CachedAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
