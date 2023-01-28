67f180baa16a4340ed55d78033a108e00a5426cd
Thomas George <private@thomas-george.com>
2023-01-28 19:07:33 +0100

    cached async image using sdwebimage

--- CachedAsyncImage/ContentView.swift
+++ CachedAsyncImage/ContentView.swift
@@ -5,15 +5,49 @@
 //  Created by Thomas George on 28/01/2023.
 //
 
+import SDWebImageSwiftUI
 import SwiftUI
 
 struct ContentView: View {
+    @State private var randomNumber = 1
+
+    let images = [
+        "https://images.igdb.com/igdb/image/upload/t_1080p/eocdvaynboqmud9hmysu.jpg",
+        "https://images.igdb.com/igdb/image/upload/t_1080p/balxsfafzeyy3ki3sc0z.jpg",
+        "https://images.igdb.com/igdb/image/upload/t_1080p/ithhctwgt8rsxbxe5uy5.jpg",
+        "https://images.igdb.com/igdb/image/upload/t_1080p/ixynqzoedwo8vc5nzqma.jpg",
+        "https://images.igdb.com/igdb/image/upload/t_1080p/bz4asonezfuq945jg1qr.jpg"
+    ]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            AsyncImage(url: URL(string: images[randomNumber])) { image in
+                image
+                    .resizable()
+                    .scaledToFill()
+                    .frame(maxWidth: .infinity, maxHeight: .infinity / 2)
+            } placeholder: {
+                ProgressView("Loading image")
+            }
+
+            WebImage(url: URL(string: images[randomNumber]))
+                .onSuccess { image, data, cacheType in
+                    print("==========")
+                    print("Size : \(image.size)")
+                    print("Data : \(data?.count)")
+                    print("Cache Type : \(cacheType.rawValue)")
+                }
+                .resizable()
+                .indicator(.progress)
+                .scaledToFill()
+                .frame(maxWidth: .infinity, maxHeight: .infinity / 2)
+
+            Button {
+                randomNumber = randomNumber == 4 ? 0 : randomNumber + 1
+            } label: {
+                Text("Change Image")
+            }
+            .buttonStyle(.bordered)
         }
         .padding()
     }
